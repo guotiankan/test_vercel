38b62f20777eedb9c2d22417d65f5a0299d5951e
Guotian Kan <42976861+guotiankan@users.noreply.github.com>
2023-12-12 00:39:06 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/rsvp', methods=['POST'])
+def rsvp():
+    name = request.form.get('name')
+    rsvp_status = request.form.get('rsvp_status')
+    dietary_preferences = request.form.get('dietary_preferences')
+
+    # Store the data in MongoDB
+    guest_data = {
+        'name': name,
+        'rsvp_status': rsvp_status,
+        'dietary_preferences': dietary_preferences
+    }
+    collection.insert_one(guest_data)
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
